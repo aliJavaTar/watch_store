1ec106e04acf221a8c273c09919cfd8a68c46c7b
alijavaboy <ali77erfagh99@gmail.com>
2022-12-02 22:06:23 +0330

    add interface watchRepo:

--- src/main/java/com/microsoft/whtch/infra/mysql/MySqlWatchRepository.java
+++ src/main/java/com/microsoft/whtch/infra/mysql/MySqlWatchRepository.java
@@ -1,18 +1,9 @@
 package com.microsoft.whtch.infra.mysql;
 
 import com.microsoft.whtch.domain.Watch;
-import com.microsoft.whtch.domain.WatchRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class MySqlWatchRepository implements WatchRepository {
-    private final JpaRepository<Watch, Long> repository;
-
-    public MySqlWatchRepository(JpaRepository<Watch, Long> repository) {
-        this.repository = repository;
-    }
-
-    @Override
-    public Watch findById(Long id) {
-        return repository.findById(id).orElseThrow(() -> new RuntimeException("not found"));
-    }
+@Repository
+public interface MySqlWatchRepository extends JpaRepository<Watch, Long> {
 }
