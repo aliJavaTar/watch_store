ccb5d03af8797bf3ec4c3ae97b4836a6865fc26a
A.erfagh <ali77erfagh99@gmail.com>
2022-12-21 10:55:59 +0330

    change controller name because they have conflict and add test but not work my mock

--- src/test/java/com/microsoft/whtch/presentation/fornt/OrderFrontControllerTest.java
+++ src/test/java/com/microsoft/whtch/presentation/fornt/OrderFrontControllerTest.java
@@ -2,9 +2,11 @@ package com.microsoft.whtch.presentation.fornt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class OrderControllerTest {
+public class OrderFrontControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
